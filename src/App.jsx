dc23b22864b78c4f784b8cf9983df7195d95dce9
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-10-29 15:27:33 +0400

    style: add consistent left margin to section components to prevent overlap with sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./components/Home/Home";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
+      <div className="sections-wrapper">
+        <Home />
+      </div>
     </>
   );
 }
