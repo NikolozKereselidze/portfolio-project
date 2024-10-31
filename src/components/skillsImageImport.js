5db8718b15218b25609b20891c80a12543782c3a
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-10-31 15:09:47 +0400

    feat: create image folders and import them in import files

---
+++ src/components/skillsImageImport.js
@@ -0,0 +1,17 @@
+import css from "../assets/skills/css.png";
+import html from "../assets/skills/html.png";
+import jest from "../assets/skills/jest.png";
+import js from "../assets/skills/js.png";
+import react from "../assets/skills/react.png";
+import webpack from "../assets/skills/webpack.png";
+
+const skillsImages = {
+  css,
+  html,
+  jest,
+  js,
+  react,
+  webpack,
+};
+
+export default skillsImages;
---
+++ src/components/webImageImport.js
@@ -0,0 +1,21 @@
+import todo from "../assets/websites/todo-list.jpg";
+import weather from "../assets/websites/weather-forecast.jpg";
+import bankist from "../assets/websites/bankist.jpg";
+import dice from "../assets/websites/dice.jpg";
+import omnifood from "../assets/websites/omnifood.jpg";
+import memorize from "../assets/websites/memorize.jpg";
+import cv from "../assets/websites/cv.jpg";
+import shopping from "../assets/websites/shopping.jpg";
+
+const webImages = {
+  todo,
+  weather,
+  bankist,
+  dice,
+  omnifood,
+  memorize,
+  cv,
+  shopping,
+};
+
+export default webImages;
