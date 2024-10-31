6e261c6fbc2a9e94e4b9b4f78813a427d6c5479a
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-10-31 19:44:18 +0400

    fix(App.jsx): fix typo when importing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Navigation from "./components/Navigation/Navigation";
-import Projects from "./components/projects/Projects";
+import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 
 function App() {
