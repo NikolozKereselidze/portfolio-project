f14717d6239e5fda708c89b55d31e455375846cd
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-10-31 15:08:14 +0400

    style: refactor code and reduce padding

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,23 +4,25 @@ const Home = () => {
   return (
     <>
       <section className={styles.homeWrapper} id="home">
-        <h4 className={styles.sectionInfo}>About Me</h4>
-        <h2 className={styles.homeGreeting}>Hello, </h2>
-        <p className={styles.homeAbout}>
-          I am a Frontend web developer specializing in dynamic and interactive
-          user interfaces, with a strong command of JavaScript, React, and Git.
-          Passionate about continuous learning and delivering polished,
-          high-performance experiences. My expertise ensures optimized and
-          efficient solutions.
-        </p>
+        <div className={styles.container}>
+          <h4 className="sectionInfo">About Me</h4>
+          <h2 className={styles.homeGreeting}>Hello, </h2>
+          <p className={styles.homeAbout}>
+            I am a Frontend web developer specializing in dynamic and
+            interactive user interfaces, with a strong command of JavaScript,
+            React, and Git. Passionate about continuous learning and delivering
+            polished, high-performance experiences. My expertise ensures
+            optimized and efficient solutions.
+          </p>
 
-        <div className={styles.buttons}>
-          <button className={styles.cvButton}>
-            <span>CV</span>
-          </button>
-          <button className={styles.hireButton}>
-            <span>Hire Me</span>
-          </button>
+          <div className={styles.buttons}>
+            <button className={styles.cvButton}>
+              <span>CV</span>
+            </button>
+            <button className={styles.hireButton}>
+              <span>Hire Me</span>
+            </button>
+          </div>
         </div>
       </section>
     </>
